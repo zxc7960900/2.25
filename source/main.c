11ad6bb18ecbdf8adcae7d3cdd3546a23b8eae3a
zxc7960900 <zxc7960900@yahoo.com>
2017-09-30 23:44:55 +0800

    adsf

---
+++ source/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("PPPPPPPPP\n");
+	printf("    P   P\n");
+	printf("    P   P\n");
+	printf("    P   P\n");
+	printf("     P P \n\n");
+	printf("  JJ\n");
+	printf(" J\n");
+	printf("J\n");
+	printf(" J\n");
+	printf("  JJJJJJJ\n\n");
+	printf("DDDDDDDDD\n");
+	printf("D       D\n");
+	printf("D       D\n");
+	printf(" D     D\n");
+	printf("  DDDDD\n");
+
+	system("pause");
+	return 0;
+}
